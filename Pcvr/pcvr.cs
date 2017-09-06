d03478fd3d30656db37c91ff68b81ef1fa23bed7
syq <shenyongqiang2008@126.com>
2017-09-06 17:14:19 +0800

    修改硬件版坐标

--- Pcvr/pcvr.cs
+++ Pcvr/pcvr.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System;
 
 public class pcvr : MonoBehaviour {
-    public static bool bIsHardWare = false;
+    public static bool bIsHardWare = true;
 	public static Vector3 CrossPositionOne;
 	public static Vector3 CrossPositionTwo;
 	public static bool IsJiaoYanHid;
@@ -1776,12 +1776,12 @@ QiNangArray[3]			QiNangArray[2]
             {
                 case PlayerEnum.PlayerOne:
                     {
-                        crossPos = MousePositionP1;
+                        crossPos = CrossPositionOne;
                         break;
                     }
                 case PlayerEnum.PlayerTwo:
                     {
-                        crossPos = MousePositionP2;
+                        crossPos = CrossPositionTwo;
                         break;
                     }
             }
